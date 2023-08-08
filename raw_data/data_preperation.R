3f5c8faa60d11fb1d09a2e4a61ded832825b2e7a
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-08-08 15:37:33 +0200

    mergin chapter

--- raw_data/data_preperation.R
+++ raw_data/data_preperation.R
@@ -1,5 +1,16 @@
 load(here::here("raw_data", "winners_raw.rda"))
 
+winners_raw <- winners_raw %>%
+  mutate("ID" = 1:nrow(.)) 
+
+## Introduce some NAs
+
+set.seed(65)
+na_rows <- sample(1:nrow(winners_raw), 5)
+  
+winners_raw[na_rows, "ID"] <- NA
+
+  
 winners <- winners_raw %>%
   select(-c("Time", "Hours", "Minutes", "Seconds"))
 
@@ -7,6 +18,6 @@ saveRDS(winners, file = here::here("raw_data", "winners.rds"))
 
 
 times <- winners_raw %>%
-  select(-c("Category", "Year", "Nationality"))
+  select(-c("Category", "Year", "Nationality", "Athlete"))
 
 saveRDS(times, file = here::here("raw_data", "times.rds"))
